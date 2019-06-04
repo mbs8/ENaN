65026cd0eb560ac47d61f04f323d6c8a3e1ee945
João Filipe Moura <jfmrm@cin.ufpe.br>
2019-06-03 22:33:58 -0300

    Refactor: changes variable name

--- algs/proposed_pre_processment/pre_processment.py
+++ algs/proposed_pre_processment/pre_processment.py
@@ -3,7 +3,7 @@ from sklearn.neighbors import KDTree
 
 def natural_neighbour(df) -> pd.DataFrame:
     kdt = KDTree(df.values, leaf_size=30, metric='euclidean')
-    nan = pd.DataFrame(columns=['true_neighbours', 'num_neighbours'])
+    nan = pd.DataFrame(columns=['natural_neighbours', 'num_neighbours'])
     
     r = 1
     num_instances_with_no_neighbour = 0
@@ -18,22 +18,22 @@ def natural_neighbour(df) -> pd.DataFrame:
         for i, row in df.iterrows():
 
             i_neighbours = kdt.query(row.values.reshape(1, -1), k=r+1, return_distance=False)
-            i_true_neighbours = []
+            i_natural_neighbours = []
             i_num_neighbours = 0
 
             for j in i_neighbours[0]:
                 if j != i:
                     j_neighbours = kdt.query(df.iloc[j].values.reshape(1, -1), k=r+1, return_distance=False)
                     if i in j_neighbours:
-                        i_true_neighbours.append(j)
+                        i_natural_neighbours.append(j)
 
             try:
-                nan.iloc[i].true_neighbours = list(set(nan.iloc[i].true_neighbours) | set(i_true_neighbours))
-                nan.iloc[i].num_neighbours = len(nan.iloc[i].true_neighbours)
+                nan.iloc[i].natural_neighbours = list(set(nan.iloc[i].natural_neighbours) | set(i_natural_neighbours))
+                nan.iloc[i].num_neighbours = len(nan.iloc[i].natural_neighbours)
             except:
                 nan = nan.append({
-                    "true_neighbours": i_true_neighbours,
-                    "num_neighbours": len(i_true_neighbours)
+                    "natural_neighbours": i_natural_neighbours,
+                    "num_neighbours": len(i_natural_neighbours)
                 }, ignore_index=True)
 
         if num_instances_with_no_neighbour == len(nan[(nan['num_neighbours'] == 0)]):
