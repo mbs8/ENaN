71b756eb328adaaeca0e7ca7ce1ac2bdaf242117
Gustavo <gustavos60@gmail.com>
2019-06-04 22:50:06 -0300

    Correção do atisa para usar 3nn

--- algs/ATISA1/atisa1.py
+++ algs/ATISA1/atisa1.py
@@ -2,21 +2,6 @@ import nn
 from sklearn.neighbors import NearestNeighbors
 from sklearn import datasets
 
-def enn(dataset, classes):
-  if (len(dataset) != len(classes)): raise Exception('Lista de classes deve ter o mesmo tamanho da lista de dados.')
-  subset_data = []
-  subset_classes = []
-  i = 0
-  n = len(dataset)
-  neighbours = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(dataset)
-  distances, indexes = neighbours.kneighbors(dataset)
-  while (i < n):
-    if (classes[i] == classes[indexes[i][1]]):
-      subset_data.append(dataset[i])
-      subset_classes.append(classes[i])
-    i += 1
-  return (subset_data, subset_classes)
-
 def calculateThresholdDistances(dataset, classes):
   if (len(dataset) != len(classes)): raise Exception('Lista de classes deve ter o mesmo tamanho da lista de dados.')
   distances = []
@@ -36,15 +21,14 @@ def subset(dataset, classes, thresholdDistances):
   subset_data.append(dataset[0])
   subset_classes.append(classes[0])
   while(i < n):
-    index, distance = nn.nearestNeighbour(dataset[i], subset_data, subset_classes)
-    if(classes[i] != classes[index] or distance > thresholdDistances[i] ):
+    classified, distance = nn.threeNN(dataset[i], classes[i], subset_data, subset_classes)
+    if( classified or distance > thresholdDistances[i] ):
       subset_classes.append(classes[i])
       subset_data.append(dataset[i])
     i += 1
   return (subset_data, subset_classes)
 
 def run(dataset, classes):
-  preprocess_data, preprocess_classes = enn(dataset, classes)
-  thresholdDistances = calculateThresholdDistances(preprocess_data, preprocess_classes)
-  subset_data, subset_classes = subset(preprocess_data, preprocess_classes, thresholdDistances)
+  thresholdDistances = calculateThresholdDistances(dataset, classes)
+  subset_data, subset_classes = subset(dataset, classes, thresholdDistances)
   return (subset_data, subset_classes)
